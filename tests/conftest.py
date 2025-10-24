118714d128fcecefa4e207eb57a16614e705b7d2
Yaroslav Petryk <ypetryk.lviv@gmail.com>
2025-10-24 10:18:20 +0300

    Add configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 import pytest
 from typing import Iterator
@@ -9,7 +10,7 @@ from helpers.opc_client import OPCClient
 load_dotenv()
 
 @pytest.fixture
-def opc_client(auth) -> Iterator[OPCClient]:
+def opc_client(auth, get_configurations) -> Iterator[OPCClient]:
     url = os.getenv("URL")
     if auth:
         user = os.getenv("USER")
@@ -29,3 +30,12 @@ def pytest_addoption(parser):
 @pytest.fixture
 def auth(request):
     return request.config.getoption("--auth")
+
+@pytest.fixture
+def get_configurations():
+    config_data = None
+    config_path = os.path.join("configs", "dev.json")
+    with open(config_path) as f:
+        config_data = json.load(f)
+    return config_data
+    
